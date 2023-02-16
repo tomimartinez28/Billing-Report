984dc1fce06d5eb0667faf37c7bd8dbe4f3cf644
Tomas Martinez <tomasagustinmartinez28@gmail.com>
2023-02-16 14:27:22 -0300

    test

--- functions.py
+++ functions.py
@@ -1,1 +1,15 @@
-def docType(file):
+
+""" file = '/Users/tomasmartinez/Documents/FACTURAS/27206517382_012_00007_00003125.pdf' """
+
+def doc_type_function(file):
+    doc_type_cod = file.split('_')[1]
+    if doc_type_cod == '211':
+        return 'FACTURA DE CREDITO ELECTRONICA MIPYMES'
+    elif doc_type_cod == '011':
+        return 'FACTURA'
+    elif doc_type_cod == '013':
+        return 'NOTA DE CREDITO'
+    elif doc_type_cod == '012':
+        return 'NOTA DE DEBITO'
+
+
--- test.py
+++ test.py
@@ -1,6 +1,67 @@
 import pdfquery
 import pandas as pd
+import os
+from functions import doc_type_function
 
-pdf = pdfquery.PDFQuery('27206517382_211_00007_00000012.pdf')
-pdf.load()
+
+
+file = '27206517382_211_00007_00000013.pdf'
+pdf = pdfquery.PDFQuery(file)
+
+if doc_type_function(file) == 'FACTURA DE CREDITO ELECTRONICA MIPYMES':
+    pdf.load()
+else:
+    pdf.load(1)
+
+print(pdf)
 pdf.tree.write('pdfXML.txt', pretty_print = True)
+
+
+
+
+
+def pdfscrape(pdf):
+
+    
+    # Extract each relevant information individually           
+    
+    date        = pdf.pq('LTTextLineHorizontal:overlaps_bbox("428.0, 724.36, 478.02, 734.36")').text()
+    period      = pdf.pq('LTTextLineHorizontal:overlaps_bbox("265.0, 654.61, 315.02, 664.61")').text()
+    os_cuit     = pdf.pq('LTTextLineHorizontal:overlaps_bbox("102.51, 635.52, 102.513, 644.52")').text().split(sep=':')[1]
+    doc_type    = doc_type_function(file)
+    punto_venta = pdf.pq('LTTextLineHorizontal:overlaps_bbox("417.0, 739.86, 444.8, 749.86")').text()
+    doc_number  = pdf.pq('LTTextLineHorizontal:overlaps_bbox("517.0, 739.86, 561.48, 749.86")').text()
+    if doc_type == 'NOTA DE CRÉDITO':
+        total       = '-' + pdf.pq('LTTextLineHorizontal:overlaps_bbox("531.31, 177.36, 573.0, 187.36")').text()
+    else:
+        total       = pdf.pq('LTTextLineHorizontal:overlaps_bbox("531.31, 177.36, 573.0, 187.36")').text()
+    detail      = pdf.pq('LTTextLineHorizontal:overlaps_bbox("57.0, 500.98, 174.32, 560.98")').text()
+
+
+
+    print('fecha  => ', date)
+    print('period  => ', (period))
+    print('Cuit  => ', os_cuit)
+    print('punto de venta  => ', punto_venta)
+    print('tipo  => ', doc_type)
+    print('Numero de Compro.  => ', doc_number)
+    print('Monto  => ', total)
+    print('detail  => ', detail)
+    
+""" # Combined all relevant information into single observation
+    page = pd.DataFrame({
+        'Fecha de Emision': date,
+        'Periodo': period,
+        'Obra Social': os_cuit,
+        'Tipo de Comprobante': doc_type,
+        'Punto de Venta': punto_venta,
+        'Numero de Comprobante': doc_number,
+        'Total': total,
+        'Detalle': detail,
+        }, index=[0])
+    return(page) """
+
+pdfscrape(pdf)
+
+
+
