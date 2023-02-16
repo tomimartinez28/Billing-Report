20ad6a6b2d493e06f845d0126557af7c0b5d7180
Tomas Martinez <tomasagustinmartinez28@gmail.com>
2023-02-16 14:39:26 -0300

    nota de credito solution

--- billingReport.py
+++ billingReport.py
@@ -1,21 +1,23 @@
 import pdfquery
 import pandas as pd
 import os
-
+from functions import doc_type_function
 
 
 
 def pdfscrape(pdf):
-
-    
-    # Extract each relevant information individually               
+    # Extract each relevant information individually
+          
     date        = pdf.pq('LTTextLineHorizontal:overlaps_bbox("428.0, 724.36, 478.02, 734.36")').text()
     period      = pdf.pq('LTTextLineHorizontal:overlaps_bbox("265.0, 654.61, 315.02, 664.61")').text()
     os_cuit     = pdf.pq('LTTextLineHorizontal:overlaps_bbox("102.51, 635.52, 102.513, 644.52")').text().split(sep=':')[1]
-    doc_type    = pdf.pq('LTTextLineHorizontal:overlaps_bbox("341.0, 760.89, 427.94, 778.89")').text()
+    doc_type    = doc_type_function(file)
     punto_venta = pdf.pq('LTTextLineHorizontal:overlaps_bbox("417.0, 739.86, 444.8, 749.86")').text()
     doc_number  = pdf.pq('LTTextLineHorizontal:overlaps_bbox("517.0, 739.86, 561.48, 749.86")').text()
-    total       = pdf.pq('LTTextLineHorizontal:overlaps_bbox("531.31, 177.36, 573.0, 187.36")').text()
+    if doc_type == 'NOTA DE CREDITO':
+        total       = '-' + pdf.pq('LTTextLineHorizontal:overlaps_bbox("531.31, 177.36, 573.0, 187.36")').text()
+    else:
+        total       = pdf.pq('LTTextLineHorizontal:overlaps_bbox("531.31, 177.36, 573.0, 187.36")').text()
     detail      = pdf.pq('LTTextLineHorizontal:overlaps_bbox("57.0, 500.98, 174.32, 560.98")').text()
 
 
@@ -43,8 +45,9 @@ def pdfscrape(pdf):
     return(page)
 
 
-directory = '/Users/tomasmartinez/Documents/FACTURAS' 
 
+
+directory = '/Users/tomasmartinez/Documents/FACTURAS' 
 master = pd.DataFrame()
 
 
@@ -52,12 +55,17 @@ master = pd.DataFrame()
 
 for file in os.listdir(directory):
     try:
-        print(file)
         pdf = pdfquery.PDFQuery(os.path.join (directory, file))
-        pdf.load(1)
+        if doc_type_function(file) == 'FACTURA DE CREDITO ELECTRONICA MIPYMES':
+            pdf.load()
+        else:
+            pdf.load(1)
         page = pdfscrape(pdf)
+        #pasar a excel
+        '''
         master = master.append(page, ignore_index=True)
         master.to_excel(f'/Users/tomasmartinez/Desktop/Archivo de Facturacion.xlsx',sheet_name='new_sheet_name', index= False)
+        '''
     except:
         print(f'Error en {file}')
 
